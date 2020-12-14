510ef1afce6602b9275be59364f1698e4a934ae6
Emanuel Trandafir <emanueltrandafir@yahoo.com>
2020-12-14 21:15:56 +0200

    *only* formatted the file

--- src/main/java/victor/training/trivia/GameBetter.java
+++ src/main/java/victor/training/trivia/GameBetter.java
@@ -4,99 +4,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameBetter implements IGame {
-	
-   public static final int BOARD_SIZE = 12;
-   private final QuestionRepository questionRepository = new QuestionRepository();
 
-   private List<Player> players = new ArrayList<>();
-   private DisplayMessages messages = new DisplayMessages();;
+	public static final int BOARD_SIZE = 12;
+	private final QuestionRepository questionRepository = new QuestionRepository();
 
-   int currentPlayerIndex = 0;
-   boolean isGettingOutOfPenaltyBox;
-   
+	private List<Player> players = new ArrayList<>();
+	private DisplayMessages messages = new DisplayMessages();;
 
-   
-   // TODO e un bug ascuns in cod. Gaseste-l
-   public void addPlayer(String playerName) {
-      players.add(new Player(playerName));
-      System.out.println( messages.getAddPlayer(playerName, players.size()) );
-   }
+	int currentPlayerIndex = 0;
+	boolean isGettingOutOfPenaltyBox;
 
-   public void roll(int roll) {
+	// TODO e un bug ascuns in cod. Gaseste-l
+	public void addPlayer(String playerName) {
+		players.add(new Player(playerName));
+		System.out.println(messages.getAddPlayer(playerName, players.size()));
+	}
 
-      System.out.println( messages.getPlayerRolledTheDice(currentPlayer().getName(), roll));
+	public void roll(int roll) {
 
-      if ( getCurrentPlayer().isInPenaltyBox() ) {
-         if (roll % 2 == 0) {
-        	 
-        	 System.out.println( messages.getPlayerNotGettingOutOfPenlatyBox(currentPlayer().getName()) );
-            isGettingOutOfPenaltyBox = false;
-            return;
-         }
-         isGettingOutOfPenaltyBox = true;
-    	 System.out.println( messages.getPlayerGettingOutOfPenlatyBox(currentPlayer().getName()) );
-      }
-      currentPlayer().advance(roll);
-      
-      System.out.println( messages.getPlayerAdvances( currentPlayer().getName(), currentPlayer().getPlace(), 
-    		  questionRepository.getCurrentCategory(currentPlayer().getPlace()).getLabel() ));
-      
-      askQuestion();
-   }
+		System.out.println(messages.getPlayerRolledTheDice(currentPlayer().getName(), roll));
 
-   private Player currentPlayer() {
-      return players.get(currentPlayerIndex);
-   }
+		if (getCurrentPlayer().isInPenaltyBox()) {
+			if (roll % 2 == 0) {
 
-   private void askQuestion() {
-      System.out.println(questionRepository.nextQuestion(currentPlayer().getPlace()));
-   }
+				System.out.println(messages.getPlayerNotGettingOutOfPenlatyBox(currentPlayer().getName()));
+				isGettingOutOfPenaltyBox = false;
+				return;
+			}
+			isGettingOutOfPenaltyBox = true;
+			System.out.println(messages.getPlayerGettingOutOfPenlatyBox(currentPlayer().getName()));
+		}
+		currentPlayer().advance(roll);
+
+		System.out.println(messages.getPlayerAdvances(currentPlayer().getName(), currentPlayer().getPlace(),
+				questionRepository.getCurrentCategory(currentPlayer().getPlace()).getLabel()));
+
+		askQuestion();
+	}
+
+	private Player currentPlayer() {
+		return players.get(currentPlayerIndex);
+	}
+
+	private void askQuestion() {
+		System.out.println(questionRepository.nextQuestion(currentPlayer().getPlace()));
+	}
 
 // TODO e un nume misleading care e ?
-   public boolean wasCorrectlyAnswered() {
-      if ( getCurrentPlayer().isInPenaltyBox() ) {
-         if (isGettingOutOfPenaltyBox) {
-        	 
-        	 getCurrentPlayer().awardCoin();
-        	 System.out.println( messages.getCorrectAnswer(currentPlayer().getName(),  getCurrentPlayer().getNumberOfCoins()) );
+	public boolean wasCorrectlyAnswered() {
+		if (getCurrentPlayer().isInPenaltyBox()) {
+			if (isGettingOutOfPenaltyBox) {
 
-            boolean winner = getCurrentPlayer().didWon();
-            currentPlayerIndex++;
-            if (currentPlayerIndex == players.size()) currentPlayerIndex = 0;
+				getCurrentPlayer().awardCoin();
+				System.out.println(
+						messages.getCorrectAnswer(currentPlayer().getName(), getCurrentPlayer().getNumberOfCoins()));
 
-            return winner;
-         } else {
-            currentPlayerIndex++;
-            if (currentPlayerIndex == players.size()) currentPlayerIndex = 0;
-            return true;
-         }
+				boolean winner = getCurrentPlayer().didWon();
+				currentPlayerIndex++;
+				if (currentPlayerIndex == players.size())
+					currentPlayerIndex = 0;
 
+				return winner;
+			} else {
+				currentPlayerIndex++;
+				if (currentPlayerIndex == players.size())
+					currentPlayerIndex = 0;
+				return true;
+			}
 
-      } else {
+		} else {
 
-    	  getCurrentPlayer().awardCoin();
-         System.out.println( messages.getCorrectAnswer(currentPlayer().getName(), getCurrentPlayer().getNumberOfCoins()) );
+			getCurrentPlayer().awardCoin();
+			System.out.println(
+					messages.getCorrectAnswer(currentPlayer().getName(), getCurrentPlayer().getNumberOfCoins()));
 
-         boolean winner = getCurrentPlayer().didWon();
-         currentPlayerIndex++;
-         if (currentPlayerIndex == players.size()) currentPlayerIndex = 0;
+			boolean winner = getCurrentPlayer().didWon();
+			currentPlayerIndex++;
+			if (currentPlayerIndex == players.size())
+				currentPlayerIndex = 0;
 
-         return winner;
-      }
-   }
+			return winner;
+		}
+	}
 
-   public boolean wrongAnswer() {
-	  System.out.println( messages.getWrongAnswer(currentPlayer().getName()) );
-      getCurrentPlayer().setInPenaltyBox( true );
+	public boolean wrongAnswer() {
+		System.out.println(messages.getWrongAnswer(currentPlayer().getName()));
+		getCurrentPlayer().setInPenaltyBox(true);
 
-      currentPlayerIndex++;
-      if (currentPlayerIndex == players.size()) currentPlayerIndex = 0;
-      return true;
-   }
+		currentPlayerIndex++;
+		if (currentPlayerIndex == players.size())
+			currentPlayerIndex = 0;
+		return true;
+	}
 
+	private Player getCurrentPlayer() {
+		return players.get(currentPlayerIndex);
+	}
 
-   private Player getCurrentPlayer() {
-	   return players.get(currentPlayerIndex);
-   }
-  
 }
